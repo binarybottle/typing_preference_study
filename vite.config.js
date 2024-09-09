37bc0b82e23f40067d3ff36b5cd5de3f89a0ec68
Arno Klein <binarybottle@gmail.com>
2024-09-09 00:07:05 -0400

    Update with vite changes for remote hosting

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 
-import { defineConfig } from 'vite';
-
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
   resolve: {
     alias: {
       'jspsych': 'node_modules/jspsych/dist/index.js',
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
     },
   },
 });
